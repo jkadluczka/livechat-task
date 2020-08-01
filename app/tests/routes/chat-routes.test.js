24f4ab80cd0c86bdcb9e5366ec5b92c8385345f9
Jacek <jackad@o2.pl>
2020-08-02 01:13:00 +0200

    app.js created, server logic splitted, tests closed properly

--- app/tests/routes/chat-routes.test.js
+++ app/tests/routes/chat-routes.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./../../../server');
+const app = require('./../../app');
 let server;
 
 beforeAll(async (done) => {
--- server.js
+++ server.js
@@ -1,12 +1,7 @@
-const express = require('express');
-const routes = require ('./app/routes')
-
-const app = express();
-routes(app);
+const app = require('./app/app')
 
 //Running server on port 8000
 app.listen(8000, () => {
   console.log('Live on port 8000');
 });
 
-module.exports = app;
